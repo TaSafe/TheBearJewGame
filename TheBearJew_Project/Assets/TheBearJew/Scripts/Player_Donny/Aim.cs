d4856246c66daae50c874dd0fe7dbaf62f8eaf4c
Caio Santos <71232338+caiossantos@users.noreply.github.com>
2021-03-23 20:15:51 -0300

    Mira básica
    - Se o collider do Chão (Layer Ground) não estiver maior que os limites da tela, a mira para de ser reconhecida

---
+++ TheBearJew_Project/Assets/TheBearJew/Scripts/Player_Donny/Aim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    [SerializeField] private LayerMask _targetLayer;
+
+    void Update() => Aiming();
+
+    private void Aiming()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, _targetLayer))
+            transform.LookAt(new Vector3(raycastHit.point.x, transform.position.y, raycastHit.point.z));
+    }
+
+}
