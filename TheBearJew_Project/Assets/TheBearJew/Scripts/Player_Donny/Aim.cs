2541b8899d9c2024cb006979706f03fe4cb2b4a9
Caio Santos <71232338+caiossantos@users.noreply.github.com>
2021-03-24 15:17:44 -0300

    Movimentação melhorada + Animações implementadas

--- TheBearJew_Project/Assets/TheBearJew/Scripts/Player_Donny/Aim.cs
+++ TheBearJew_Project/Assets/TheBearJew/Scripts/Player_Donny/Aim.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Aim : MonoBehaviour
 {
     [SerializeField] private LayerMask _targetLayer;
+    private Camera _camera;
+
+    private void Start() => _camera = Camera.main;
 
     void Update() => Aiming();
 
     private void Aiming()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, _targetLayer))
-            transform.LookAt(new Vector3(raycastHit.point.x, transform.position.y, raycastHit.point.z));
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, float.MaxValue, _targetLayer))
+        {
+            transform.LookAt(new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z));    // Old code to make character aim
+            
+            // The new code don't seen to be most effective then the old one
+            //var _aimDirection = hitInfo.point - transform.position;
+            //_aimDirection.y = 0f;
+            //_aimDirection.Normalize();
+            //transform.forward = _aimDirection;
+        }
     }
 
 }
--- TheBearJew_Project/Assets/TheBearJew/Scripts/Player_Donny/Movement.cs
+++ TheBearJew_Project/Assets/TheBearJew/Scripts/Player_Donny/Movement.cs
@@ -2,47 +2,69 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float _characterSpeed = 5f;
-    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private float _characterSpeed = 3f;
+    [SerializeField] private float _gravity = -22f;
 
     [Header("Ground Checking")]
     [SerializeField] private LayerMask _groundLayer;
-    [SerializeField] private Vector3 _checkPosition;
-    [SerializeField] private float _checkRadius;
+    [SerializeField] private Vector3 _checkSpherePosition;
+    [SerializeField] private float _checkSphereRadius;
 
     private CharacterController _characterController;
-    private Vector3 playerVelocity = Vector3.zero;  //Up Down velocity
+    private Vector3 playerVelocity = Vector3.zero;  // Up/Down movement velocity
+    
+    //Animation
+    private Animator _animator;
 
-    private void Start() => _characterController = GetComponent<CharacterController>();
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _animator = GetComponentInChildren<Animator>();
+    }
 
     private void Update() => Move();
 
     private void Move()
     {
         //Check gravity
-        var isGrounded = Physics.CheckSphere(_checkPosition + transform.position, _checkRadius, _groundLayer);  //verify if is in the ground
+        bool isGrounded = Physics.CheckSphere(_checkSpherePosition + transform.position, _checkSphereRadius, _groundLayer);  //verify if is in the ground
         
         if (isGrounded && playerVelocity.y < 0f)
             playerVelocity.y = -2f;
 
         //Player Input
-        var xInput = Input.GetAxisRaw("Horizontal");
-        var yInput = Input.GetAxisRaw("Vertical");
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
 
         //Final movement calculations
-        var move = new Vector3(xInput, 0f, yInput);
-        _characterController.Move(move * _characterSpeed * Time.deltaTime);
+        Vector3 move = new Vector3(xInput, 0f, yInput);
+
+        if (move.magnitude > 0)
+        {
+            move.Normalize();  //This part is responsible for make the diagonals become part of the circle of values and not de addition (1,1) -> (0.707,0.0707)
+            _characterController.Move(move * _characterSpeed * Time.deltaTime);
+        }
 
         //Aplly gravity
         playerVelocity.y += _gravity * Time.deltaTime;
         _characterController.Move(playerVelocity * Time.deltaTime);
+
+        //## Animations ##
+
+        //Calcula o valor do movimento de acordo com a alteração da direção
+        float velocityX = Vector3.Dot(move.normalized, transform.right);
+        float velocityZ = Vector3.Dot(move.normalized, transform.forward);
+
+        //Altera os valores na animação
+        _animator.SetFloat("xVelocity", velocityX, .1f, Time.deltaTime);
+        _animator.SetFloat("zVelocity", velocityZ, .1f, Time.deltaTime);
     }
 
     private void OnDrawGizmos()
     {
         //Check gravity gizmos
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(_checkPosition + transform.position, _checkRadius);
+        Gizmos.DrawWireSphere(_checkSpherePosition + transform.position, _checkSphereRadius);
     }
 
 }
