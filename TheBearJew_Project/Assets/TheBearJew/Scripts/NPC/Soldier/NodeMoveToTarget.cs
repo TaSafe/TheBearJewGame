551d91f8d7ac007876a0137df33fb6622fd5e2e3
Caio Santos <71232338+caiossantos@users.noreply.github.com>
2021-05-19 20:42:21 -0300

    +Inicio da movimentação até alvo via BT usando NavMesh

---
+++ TheBearJew_Project/Assets/TheBearJew/Scripts/NPC/Soldier/NodeMoveToTarget.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NodeMoveToTarget : BTNode
+{
+
+    private NavMeshAgent _navMeshAgent;
+    private List<GameObject> _targets = new List<GameObject>();
+    private GameObject nullTarget;
+
+    public NodeMoveToTarget(Transform defaultPosition, NavMeshAgent navMeshAgent, List<GameObject> targets)
+    {
+        _navMeshAgent = navMeshAgent;
+
+        //if (targets == null)
+        //{
+        //    Vector3 newPos = new Vector3(defaultPosition.position.x, defaultPosition.position.y, defaultPosition.position.z);
+        //    nullTarget = GameObject.Instantiate(new GameObject(), newPos, Quaternion.identity);
+        //    _targets.Add(nullTarget);
+        //}
+        //else
+            _targets = targets;
+    }
+
+    public override IEnumerator Run(BTRoot root)
+    {
+        status = Status.RUNNING;
+
+        _navMeshAgent.SetDestination(_targets[0].transform.position);
+
+        while (_navMeshAgent.remainingDistance > .5f)
+        {
+            yield return null;
+
+            status = Status.SUCCESS;
+        }
+
+        if (status == Status.RUNNING) status = Status.FAILURE;
+
+        Print();
+    }
+}
