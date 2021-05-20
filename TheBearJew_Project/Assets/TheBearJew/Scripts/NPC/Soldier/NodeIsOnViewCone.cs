172c46421e931bb5cd9b7cceefbe0d218c5530a9
Caio Santos <71232338+caiossantos@users.noreply.github.com>
2021-05-20 17:32:24 -0300

    Create NodeIsOnViewCone.cs

---
+++ TheBearJew_Project/Assets/TheBearJew/Scripts/NPC/Soldier/NodeIsOnViewCone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NodeIsOnViewCone : BTNode
+{
+    private float _coneAngle;
+
+    public NodeIsOnViewCone(float coneAngle) => _coneAngle = coneAngle;
+
+    public override IEnumerator Run(BTRoot root)
+    {
+        status = Status.FAILURE;
+
+        if (MathUtilities.ConeChecker(root.gameObject, PlayerInput.Instance.gameObject.transform.position, _coneAngle))
+            status = Status.SUCCESS;
+
+        yield break;
+    }
+}
