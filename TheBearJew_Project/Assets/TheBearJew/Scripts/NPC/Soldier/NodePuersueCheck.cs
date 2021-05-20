882f73b6064af4294d636ca4d773cae3e81e1796
Caio Santos <71232338+caiossantos@users.noreply.github.com>
2021-05-20 15:12:33 -0300

    +Node para verificar se o inimigo estava perseguindo o Player

---
+++ TheBearJew_Project/Assets/TheBearJew/Scripts/NPC/Soldier/NodePuersueCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+public class NodePuersueCheck : BTNode
+{
+    public override IEnumerator Run(BTRoot root)
+    {
+        status = Status.FAILURE;
+
+        if (root.GetComponent<Enemy>().PursueState)
+            status = Status.SUCCESS;
+
+        yield break;
+    }
+}
