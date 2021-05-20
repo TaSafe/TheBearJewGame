75634c7a4c26dbf85fb1c264437a9ef861ee4b51
Caio Santos <71232338+caiossantos@users.noreply.github.com>
2021-05-20 15:13:23 -0300

    +Node para verificar se o player entrou na área próxima à IA

---
+++ TheBearJew_Project/Assets/TheBearJew/Scripts/NPC/Soldier/NodeGameObjectNear.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NodeGameObjectNear : BTNode
+{
+    private float circleRadius;
+    private GameObject gameObjectToCheck;
+
+    public NodeGameObjectNear(float parameters, GameObject go)
+    {
+        circleRadius = parameters;
+        gameObjectToCheck = go;
+    }
+
+    public override IEnumerator Run(BTRoot root)
+    {
+        status = Status.FAILURE;
+
+        if (Physics.SphereCast(new Ray(root.transform.position, root.transform.forward), circleRadius, out RaycastHit hit))
+        {
+            if (hit.collider.gameObject == gameObjectToCheck)
+                status = Status.SUCCESS;
+        }
+
+        Print();
+
+        yield break;
+    }
+}
