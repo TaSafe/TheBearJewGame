aa11cd00add250bcfa09fe5255dd9b795887175b
Caio Santos <71232338+caiossantos@users.noreply.github.com>
2021-05-20 17:30:51 -0300

    Classe MathUtilities criada para abrigar métodos de cálculo matemático genéricos

---
+++ TheBearJew_Project/Assets/TheBearJew/Scripts/MathUtilities.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MathUtilities : MonoBehaviour
+{
+    public static bool ConeChecker(GameObject mainGameObject, Vector3 objectToCompare, float coneAngle)
+    {
+        float cosAngle = Vector3.Dot(
+            (objectToCompare - mainGameObject.transform.position).normalized,
+            mainGameObject.transform.forward);
+
+        float angle = Mathf.Acos((cosAngle) * Mathf.Rad2Deg);
+
+        return angle < coneAngle;
+    }
+}
