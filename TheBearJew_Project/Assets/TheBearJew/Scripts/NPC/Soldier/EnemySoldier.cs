1d8c89c6f86a1a89adfbb8ba131a17b25d03923c
Caio Santos <71232338+caiossantos@users.noreply.github.com>
2021-05-21 16:00:58 -0300

    Novos métodos necessários implementados

--- TheBearJew_Project/Assets/TheBearJew/Scripts/NPC/Soldier/EnemySoldier.cs
+++ TheBearJew_Project/Assets/TheBearJew/Scripts/NPC/Soldier/EnemySoldier.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class EnemySoldier : Enemy
 {
     [SerializeField] private EnemyDataSO _enemyData;
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private Transform _muzzle;
 
     [Header("Patrol Points")]
     [SerializeField] private bool _movingPatrol = true;
@@ -17,15 +18,23 @@ public class EnemySoldier : Enemy
     [SerializeField] private float _detectionConeAngle = 30f;
 
     private BTRoot behaviour;
+    
+    //Talvez saia depois
+    private bool _shoot;
 
     private void Awake() => EnemyInit(_enemyData);
 
-    private void Start()
+    private void Start() => BehaviourTree();
+
+    private void Update() => PursueState = _isPursuing;
+
+    public override void BehaviourTree()
     {
         behaviour = GetComponent<BTRoot>();
 
         BTSequence patrol = new BTSequence();
-        patrol.children.Add( new NodeApproximationToPlayer(3f, GetComponent<NavMeshAgent>()) );
+        patrol.children.Add(new NodeAttack());
+        //patrol.children.Add( new NodeApproximationToPlayer(3f, GetComponent<NavMeshAgent>()) );
 
         //O if é para facilitar durante o desenvolvimento, não afeta em nada a lógica
         //if (_movingPatrol)
@@ -35,11 +44,6 @@ public class EnemySoldier : Enemy
 
         behaviour.root = patrol;
         StartCoroutine(behaviour.Execute());
-    }
-
-    private void Update()
-    {
-        PursueState = _isPursuing;
     }
 
     public override void Damage(float damage, bool bat = false) 
@@ -59,9 +63,27 @@ public class EnemySoldier : Enemy
         }
     }
 
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.DrawWireSphere(transform.position, _detectionRadius);
-    //}
+    public override void Attack()
+    {
+        Vector3 playerPosition = PlayerInput.Instance.transform.position;
+        Vector3 toLook = new Vector3(playerPosition.x, transform.position.y, playerPosition.z);
+        transform.LookAt(toLook);
 
+        //shoot
+        if (!_shoot)
+            StartCoroutine(Shoot());
+    }
+
+    private IEnumerator Shoot()
+    {
+        Vector3 bulletSpawnPosition = new Vector3(transform.position.x, 1.5f, transform.position.z * .8f);
+        Instantiate(_bullet, _muzzle.position, transform.rotation);
+        _shoot = true;
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Guns/enemy_shot"); //som tiro
+
+        yield return new WaitForSeconds(.7f);
+
+        _shoot = false;
+    }
+    
 }
