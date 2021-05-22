287838754465ceda06f573ee1114a4759253820c
Caio Santos <71232338+caiossantos@users.noreply.github.com>
2021-05-22 18:12:42 -0300

    Sistema Key->Gate melhorado e agora funciona entre cenas

--- TheBearJew_Project/Assets/TheBearJew/Scripts/J2 Temp/Gate.cs
+++ TheBearJew_Project/Assets/TheBearJew/Scripts/J2 Temp/Gate.cs
@@ -2,18 +2,30 @@
 
 public abstract class Gate : MonoBehaviour
 {
-    [SerializeField] private GameObject _key;
+    [SerializeField] private string _keyName;
 
     public abstract void GateActions();
 
-    public bool CheckKey()
+    public bool CheckKeyInPlayerInventary()
     {
-        if (PlayerInput.Instance.Inventory.ContainItem(_key))
+        if (PlayerInput.Instance.Inventory.HasItemOfType<Key>(out Key key))
         {
-            return true;
+            if (key.KeyName == _keyName)
+                return true;
         }
-        else
-            return false;
+        
+        return false;
+    }
+
+    public void RemoveKeyFromPlayerInventary()
+    {
+        Key key;
+        if (PlayerInput.Instance.Inventory.HasItemOfType<Key>(out key))
+        {
+            PlayerInput.Instance.Inventory.RemoveItem(key.gameObject);
+            key.gameObject.transform.parent = null;
+            Destroy(key.gameObject);
+        }
     }
 
 }
--- TheBearJew_Project/Assets/TheBearJew/Scripts/J2 Temp/Key.cs
+++ TheBearJew_Project/Assets/TheBearJew/Scripts/J2 Temp/Key.cs
@@ -3,16 +3,22 @@ using UnityEngine.Events;
 
 public class Key : MonoBehaviour, IInteraction
 {
+    [SerializeField] private string _keyName;
     [SerializeField] private Sprite _keyHudImage;
     [SerializeField] private UnityEvent OnCollect;
+
+    public string KeyName { get { return _keyName; } }
 
     public void IdleInteraction() { }
     public void Interacting() { }
     public void Interaction()
     {
-        PlayerInput.Instance.Inventory.AddItem(gameObject);
-        gameObject.transform.position = new Vector3(0f, 1000f, 0f);
-        OnCollect?.Invoke();
+        if (!PlayerInput.Instance.Inventory.ContainItem(gameObject))
+            PlayerInput.Instance.Inventory.AddItem(gameObject);
+
         gameObject.SetActive(false);
+        gameObject.transform.position = new Vector3(0f, 1000f, 0f);
+        gameObject.transform.SetParent(PlayerInput.Instance.transform);
+        OnCollect?.Invoke();
     }
 }
